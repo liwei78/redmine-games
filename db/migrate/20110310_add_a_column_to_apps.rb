fc37818c3f16a6e48024dd49084551ea02de566b
liwei <hi@liwei.me>
2012-01-10 11:25:04 +0800

    uncomment

--- db/migrate/20110310_add_a_column_to_apps.rb
+++ db/migrate/20110310_add_a_column_to_apps.rb
@@ -1,9 +1,9 @@
 class AddAColumnToApps < ActiveRecord::Migration
   def self.up
-    # add_column :games_apps, :icon_style, :integer, :default => 0
+    add_column :games_apps, :icon_style, :integer, :default => 0
   end
 
   def self.down
-    # remove_column :games_apps, :icon_style
+    remove_column :games_apps, :icon_style
   end
 end
